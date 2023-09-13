292d4e9f364c89f7629cb31e8072ddd80373a537
DuduViolante <126472870+DuduViolante@users.noreply.github.com>
2023-09-13 16:34:17 -0300

    apostila 2 feita

---
+++ my-creat-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  const aluno = 'eduardo'
+  const curso = 'ads'
+
+      return (
+        <>
+        <h1>oi</h1>
+        </>
+      )
+    }
